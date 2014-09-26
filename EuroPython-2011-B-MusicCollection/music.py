d9d7fe3209d89bae998460979f28e96aeaab6dde
Leo Antoli <leo.antoli@gmail.com>
2014-09-27 01:28:55 +0200

    music collection

---
+++ EuroPython-2011-B-MusicCollection/music.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python3
+
+"""
+Solution to: https://code.google.com/codejam/contest/1277486/dashboard#s=p1
+"""
+
+FILENAME = "B-small-practice-2"
+
+import sys
+sys.stdin = open(FILENAME + ".in", 'r')
+sys.stdout = open(FILENAME + ".out", 'w')
+
+
+def optimal(song, songs):
+    if not songs: return '""'
+    for size in range(1, len(song)+1):
+        candidates = [song[i:(i+size)] for i in range(len(song)+1 - size)
+                if not any([song[i:(i+size)] in other for other in songs])]
+        if candidates:
+            return '"' + sorted(candidates)[0] + '"'
+    return ':('
+
+
+def solve(songs):
+    return [optimal(song, songs[:i] + songs[(i+1):]) for i, song in enumerate(songs)]
+
+
+def get_line(): return input()
+def get_int(): return int(get_line())
+
+
+if __name__ == '__main__':
+    for case in range(get_int()):
+        print('Case #{}:\n{}'.format(case+1,
+                '\n'.join(solve([get_line().upper() for _ in range(get_int())]))))
