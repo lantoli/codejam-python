4bbe5f615d478fbd866c35d56d0f77d9a9501af9
Leo Antoli <leo.antoli@gmail.com>
2014-09-28 12:37:53 +0200

    refactored music collection

--- EuroPython-2011-B-MusicCollection/music.py
+++ EuroPython-2011-B-MusicCollection/music.py
@@ -11,18 +11,18 @@ sys.stdin = open(FILENAME + ".in", 'r')
 sys.stdout = open(FILENAME + ".out", 'w')
 
 
-def optimal(song, songs):
-    if not songs: return '""'
-    for size in range(1, len(song)+1):
-        candidates = [song[i:(i+size)] for i in range(len(song)+1 - size)
-                if not any([song[i:(i+size)] in other for other in songs])]
+def optimal(song, others):
+    if not others: return '""'
+    for size in range(1, len(song) + 1):
+        candidates = [song[i:(i + size)] for i in range(len(song) + 1 - size)
+                      if all([song[i:(i + size)] not in other for other in others])]
         if candidates:
             return '"' + sorted(candidates)[0] + '"'
     return ':('
 
 
 def solve(songs):
-    return [optimal(song, songs[:i] + songs[(i+1):]) for i, song in enumerate(songs)]
+    return [optimal(song, songs[:i] + songs[(i + 1):]) for i, song in enumerate(songs)]
 
 
 def get_line(): return input()
@@ -31,5 +31,5 @@ def get_int(): return int(get_line())
 
 if __name__ == '__main__':
     for case in range(get_int()):
-        print('Case #{}:\n{}'.format(case+1,
-                '\n'.join(solve([get_line().upper() for _ in range(get_int())]))))
+        print('Case #{}:\n{}'.format(case + 1,
+                    '\n'.join(solve([get_line().upper() for _ in range(get_int())]))))
